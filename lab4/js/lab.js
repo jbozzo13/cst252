d7031b3a77ad16b1342344588af58217bc8967d1
jbozzo13 <70286102+jbozzo13@users.noreply.github.com>
2021-02-10 14:23:58 -0800

    lab saves

---
+++ lab4/js/lab.js
@@ -0,0 +1,10 @@
+// How to open your inventory in game
+// Press 'E' to trigger inventory opening
+// Print the grid for items to go into
+    // Loop through each row
+      // Print each item or print space if slot is empty
+      // Print a | between
+    // Print a newline at the end
+    // Print a horizontal line in between
+
+// Credit to Wes Modes' Tic Tac Toe pseudocode
